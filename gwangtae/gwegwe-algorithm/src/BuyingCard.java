95494e43fe09088c36e500e98e56a9d6ad53db4e
gwegwe1234 <gwangtae5860@daum.net>
2020-10-19 19:10:13 +0900

    (광태) 알고리즘 기초 1 ~ 이친수까지

---
+++ gwangtae/gwegwe-algorithm/src/BuyingCard.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BuyingCard {
+
+  static int[] d;
+  static int[] p;
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    d = new int[n+2];
+    p = new int[n+2];
+    for (int i = 1; i <= n; i++) {
+      p[i] = sc.nextInt();
+    }
+    d[0] = 0;
+    d[1] = p[1];
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        d[i] = Math.max(d[i], d[i-j] + p[j]);
+      }
+    }
+
+    System.out.println(d[n]);
+    sc.close();
+  }
+
+}
---
+++ gwangtae/gwegwe-algorithm/src/EasyStairNumber.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EasyStairNumber {
+
+  static int[][] d;
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    d = new int[n+1][10];
+
+    // 초기값
+    for (int i = 1; i <= 9; i++) {
+      d[1][i] = 1;
+    }
+
+    for (int i = 2; i<= n; i++) {
+      for (int j = 0; j <= 9; j++) {
+        if (j-1 >= 0) {
+          d[i][j] += d[i-1][j-1];
+        }
+
+        if (j+1 <=9) {
+          d[i][j] += d[i-1][j+1];
+        }
+
+        d[i][j] %= 1000000000;
+      }
+    }
+
+    Long result = 0L;
+    for (int i = 0; i <= 9; i++) {
+      result += Long.valueOf(d[n][i]);
+    }
+    result %= 1000000000;
+
+    System.out.println(result);
+    sc.close();
+  }
+}
