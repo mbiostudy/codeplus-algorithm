9264d86c5e72b4e16ef08f90626afe1c93f00cb1
gwegwe1234 <gwangtae5860@daum.net>
2020-11-21 14:35:38 +0900

    (광태) 알고리즘 기초 1 ~ 연습까지

---
+++ gwangtae/gwegwe-algorithm/src/IntegerTriangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IntegerTriangle {
+
+  static int[][] d;
+  static int[][] a;
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    a = new int[n+1][n+1];
+    d = new int[n+1][n+1];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        a[i][j] = sc.nextInt();
+      }
+    }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (d[i-1][j] > d[i-1][j-1]) {
+          d[i][i] += d[i-1][j] + a[i][j];
+        } else {
+          d[i][i] += d[i-1][j-1] + a[i][j];
+        }
+      }
+    }
+  }
+}
---
+++ gwangtae/gwegwe-algorithm/src/UpUpUpNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UpUpUpNumbers {
+  static int[][] d;
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    d = new int[n+1][10];
+
+    for (int i = 0; i < 10; i++) {
+      d[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 0; j <= 9; j++) {
+        for (int k = 0; k <=j; k++) {
+          d[i][j] += d[i-1][k];
+        }
+      }
+    }
+  }
+}
