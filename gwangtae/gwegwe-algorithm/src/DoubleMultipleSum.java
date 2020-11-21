38e9c4b004269b9cc521a3d979e5f7d5b89a602c
gwegwe1234 <gwangtae5860@daum.net>
2020-11-21 12:54:05 +0900

    (광태) 알고리즘 기초 1 ~ 합분해까지

---
+++ gwangtae/gwegwe-algorithm/src/DoubleMultipleSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DoubleMultipleSum {
+
+  static int[] d;
+  static int[] a;
+
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    a = new int[n+1];
+    d = new int[n+1];
+
+    for (int i = 1; i <= n; i++) {
+      d[i] = i;
+      for (int j = 1; j * j <= i; j++) {
+        if (d[i] > d[i-j*j] + 1) {
+          d[i] = d[i-j*j] + 1;
+        }
+      }
+    }
+
+    System.out.println(d[n]);
+    sc.close();
+  }
+}
---
+++ gwangtae/gwegwe-algorithm/src/SequentialSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SequentialSum {
+
+  static int[] a;
+  static int[] d;
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    a = new int[n+1];
+    d = new int[n+1];
+
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+
+    d[0] = a[0];
+    for (int i = 0; i < n - 1; i++) {
+      if (a[i+1] > d[i] + a[i+1]) {
+        d[i+1] = a[i+1];
+      } else {
+        d[i+1] = a[i+1] + d[i];
+      }
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+      if (d[i] > result) {
+        result = d[i];
+      }
+    }
+
+    System.out.println(result);
+    sc.close();
+  }
+}
