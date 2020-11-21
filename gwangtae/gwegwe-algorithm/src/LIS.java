467ba13e169cfde3246b6902d212237562b0c95e
gwegwe1234 <gwangtae5860@daum.net>
2020-11-21 12:54:05 +0900

    (광태) 알고리즘 기초 1 ~ 합분해까지

---
+++ gwangtae/gwegwe-algorithm/src/LIS.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LIS {
+
+  static int[] a;
+  static int[] d;
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    a = new int[n+1];
+    d = new int[n+1];
+
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+      d[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (a[j] < a[i] && d[i] < d[j] + 1) {
+          d[i] = d[j] + 1;
+        }
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (d[i] > result) {
+        result = d[i];
+      }
+    }
+
+    System.out.println(result);
+    sc.close();
+  }
+}
